70f721f59c0f16cef445ca0056f9b78d86216f49
roxy93 <roxy15.12@gmail.com>
2018-09-06 18:06:36 +0200

    genera documento con los tiempos

--- app/src/main/java/com/vikramezhil/droidspeechexample/Activity_DroidSpeech.java
+++ app/src/main/java/com/vikramezhil/droidspeechexample/Activity_DroidSpeech.java
@@ -14,8 +14,19 @@ import com.vikramezhil.droidspeech.DroidSpeech;
 import com.vikramezhil.droidspeech.OnDSListener;
 import com.vikramezhil.droidspeech.OnDSPermissionsListener;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
+
+import static com.vikramezhil.droidspeechexample.Function.createFile;
+import static com.vikramezhil.droidspeechexample.Function.write;
+import static com.vikramezhil.droidspeechexample.Function.writeToSDFile;
+import static com.vikramezhil.droidspeechexample.Function.msToString;
 
 /**
  * Droid Speech Example Activity
@@ -30,6 +41,16 @@ public class Activity_DroidSpeech extends Activity implements OnClickListener, O
     private DroidSpeech droidSpeech;
     private TextView finalSpeechResult;
     private ImageView start, stop;
+    private Boolean newFile = true;
+    private Boolean firstWord = true;
+    private long tFin = 0;
+    private long tIni = 0;
+    private long timeTotal =0;
+    private long seg = 0;
+    private int min = 0;
+    private int hora = 0;
+    private String tiempo = "";
+    File file = createFile ();
 
     // MARK: Activity Methods
 
@@ -58,6 +79,7 @@ public class Activity_DroidSpeech extends Activity implements OnClickListener, O
 
         stop = findViewById(R.id.stop);
         stop.setOnClickListener(this);
+
     }
 
     @Override
@@ -97,7 +119,7 @@ public class Activity_DroidSpeech extends Activity implements OnClickListener, O
                 // Setting the view visibilities when droid speech is running
                 start.setVisibility(View.GONE);
                 stop.setVisibility(View.VISIBLE);
-
+                tIni = System.currentTimeMillis();//tiempo de referencia
                 break;
 
             case R.id.stop:
@@ -134,13 +156,29 @@ public class Activity_DroidSpeech extends Activity implements OnClickListener, O
     @Override
     public void onDroidSpeechRmsChanged(float rmsChangedValue)
     {
-        // Log.i(TAG, "Rms change value = " + rmsChangedValue);
+         Log.i(TAG, "Rms change value = " + rmsChangedValue);
     }
 
     @Override
     public void onDroidSpeechLiveResult(String liveSpeechResult)
     {
         Log.i(TAG, "Live speech result = " + liveSpeechResult);
+
+        if(newFile){
+            write(file); //crear por primera vez el archivo
+            newFile = false;
+        }
+
+        if (firstWord) {// primera palabra para reconocer
+
+            tFin = System.currentTimeMillis();//ver si debo cambiar los 1000
+            timeTotal = tFin - tIni;
+            tiempo = msToString(timeTotal, true);
+            writeToSDFile(tiempo,file);
+            firstWord = false;
+        }
+
+        //tFin = System.currentTimeMillis(); //tiempo final
     }
 
     @Override
@@ -148,6 +186,17 @@ public class Activity_DroidSpeech extends Activity implements OnClickListener, O
     {
         // Setting the final speech result
         this.finalSpeechResult.setText(finalSpeechResult);
+
+        firstWord = true;//la siguiente palabra es la primera de un nuevo párrafo
+
+        if (finalSpeechResult != "") {//words2
+            //writeToSDFile(" S" + hora + ":" + min + ":" + seg + " ",file);
+            tFin = System.currentTimeMillis();
+            timeTotal = tFin - tIni;
+            tiempo = msToString(timeTotal, false);
+            writeToSDFile(finalSpeechResult + " ",file);
+            writeToSDFile(tiempo + '\n',file);
+        }
 
         if(droidSpeech.getContinuousSpeechRecognition())
         {
@@ -223,5 +272,90 @@ public class Activity_DroidSpeech extends Activity implements OnClickListener, O
                 }
             });
         }
+    }
+
+}
+
+class Function {
+
+    public static File createFile (){
+        File root = android.os.Environment.getExternalStorageDirectory();
+
+        File dir = new File (root.getAbsolutePath() + "/folder");
+
+        if (dir.exists()){
+            dir.delete();
+        }
+        dir.mkdirs();
+
+        File file = new File(dir, "text.txt");
+
+        if (dir.exists()){
+            file.delete();
+        }
+
+        return file;
+    }
+
+    public static void writeToSDFile(String speechToTextData, File file){
+
+        // Find the root of the external storage.
+        // See http://developer.android.com/guide/topics/data/data-  storage.html#filesExternal
+
+        try {
+            FileWriter fw = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(speechToTextData);
+            bw.flush();
+            bw.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void write (File file) {
+        try {
+            FileOutputStream f = new FileOutputStream(file);
+            f.write("".getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String msToString (long ms, Boolean start) {
+        long seg = 0;
+        int min = 0;
+        int hora = 0;
+        seg = ms/1000;
+        String tiempo= "";
+
+        if (seg<60){
+            min = 0;
+        }
+        else{
+            min = (int) (seg/60);
+            seg = seg - (min*60);
+        }
+
+        if (min<60){
+            hora = 0;
+
+        }
+        else{
+            hora = (min/60);
+            min = min - (hora*60);
+        }
+        if (start==true){
+            tiempo = " S" + hora + ":" + min + ":" + seg + " ";
+        }else{
+            tiempo = " F" + hora + ":" + min + ":" + seg + " ";
+        }
+
+        return tiempo;
     }
 }
